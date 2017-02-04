d15cc58cbfe172325121d8223b96d1bbbc756a2d
Shell Bryson <shell@shellbryson.com>
2017-02-04 18:16:55 +0000

    Relocates search into Tile Menu. Makes search / menu independently toggle.

--- js/functions.js
+++ js/functions.js
@@ -5,21 +5,21 @@
 (function( $ ) {
 
   var body, resizeTimer;
-  var $nav = $(".js-navigation");
-  var $search = $(".js-search");
+  var $nav = $('.js-navigation');
+  var $search = $('.js-search');
   var $toggleMenu = $('.js-toggleMenu');
   var $toggleSearch = $('.js-toggleSearch');
 
   function initNavigation() {
-
     $toggleMenu.on('click', function (e) {
       e.preventDefault();
       $nav.toggleClass('su-navigation__menu--show');
+      $search.toggleClass('su-navigation-search--show');
     });
 
     $toggleSearch.on('click', function (e) {
       e.preventDefault();
-      $search.toggleClass('su-search--show');
+      $search.toggleClass('su-navigation-search--show');
     });
   }
 
