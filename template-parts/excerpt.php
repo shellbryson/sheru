7295b86d2ff06d9807c3cc2ec5ba587a685f140a
Shell Bryson <shell@shellbryson.com>
2017-09-30 22:07:37 +0100

    Adds front page / default excerpts with styling

---
+++ template-parts/excerpt.php
@@ -0,0 +1,19 @@
+<article class="su-article" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+  <header class="su-header su-article__header">
+    <h2 class="su-heading su-heading--two su-article__title">
+      <a href="<?php esc_url( get_permalink() ) ?>" rel="bookmark" class="su-heading__link">
+        <?php the_title() ?>
+      </a>
+    </h2>
+    <div class="su-article__date"><?php the_date() ?></div>
+  </header>
+  <section class="su-article__content">
+    <?php
+      the_excerpt();
+    ?>
+    <a href="<?php esc_url( get_permalink() ) ?>" class="su-article__more">Continue reading <span class="su-article__more-sr-only"><?php the_title() ?></span></a>
+  </section>
+  <footer class="su-article__footer">
+    <?php sheru_entry_meta(); ?>
+  </footer>
+</article>
