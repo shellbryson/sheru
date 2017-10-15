f4bf3586fbfcab856181c3cde3885b3e831f7797
Shell Bryson <shell@shellbryson.com>
2017-10-15 23:00:07 +0100

    Adds templates for devlogs so that they actually display...

---
+++ archive-devlogs.php
@@ -0,0 +1,32 @@
+<?php
+get_header(); ?>
+
+<main class="su-content su-content--devlog" id="primary">
+
+  <?php if ( have_posts() ) : ?>
+
+    <header class="su-page-header">
+      <h1 class="su-heading su-heading--one"><?php the_archive_title(); ?></h1>
+      <h2 class="su-heading__subheading"><?php the_archive_description(); ?></h2>
+    </header>
+
+    <?php
+    while ( have_posts() ) : the_post();
+      get_template_part( 'template-parts/content-devlog', get_post_format() );
+    endwhile;
+
+    the_posts_pagination( array(
+      'prev_text'          => __( 'Previous page', 'sheru' ),
+      'next_text'          => __( 'Next page', 'sheru' ),
+      'before_page_number' => '<span class="su-post-navigation sr-only">' . __( 'Page', 'sheru' ) . ' </span>',
+    ) );
+
+  else :
+    get_template_part( 'template-parts/content', 'none' );
+  endif;
+  ?>
+
+</main>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
---
+++ template-parts/content-devlog.php
@@ -0,0 +1,46 @@
+<article class="su-article su-article--devlog" id="post-<?php the_ID(); ?>">
+
+  <header class="su-header su-article__header">
+    <?php if ( is_single() ) : ?>
+      <?php the_title( sprintf( '<h1 class="su-heading su-heading--one su-article__title"><a href="%s" rel="bookmark" class="su-heading__link">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+    <?php else : ?>
+      <?php the_title( sprintf( '<h2 class="su-heading su-heading--two su-article__title"><a href="%s" rel="bookmark" class="su-heading__link">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+    <?php endif; ?>
+  </header>
+
+  <?php sheru_post_thumbnail(); ?>
+
+  <section class="su-article__content">
+    <?php
+
+      the_content();
+
+      wp_link_pages( array(
+        'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'sheru' ) . '</span>',
+        'after'       => '</div>',
+        'link_before' => '<span>',
+        'link_after'  => '</span>',
+        'pagelink'    => '<span class="sr-only">' . __( 'Page', 'sheru' ) . ' </span>%',
+        'separator'   => '<span class="sr-only">, </span>',
+      ) );
+    ?>
+  </section>
+
+  <footer class="su-article__footer">
+
+    <div  class="su-article__meta">
+      <?php sheru_entry_meta(); ?>
+    </div>
+    <?php
+      edit_post_link(
+        sprintf(
+          /* translators: %s: Name of current post */
+          __( 'Edit<span class="sr-only"> "%s"</span>', 'sheru' ),
+          get_the_title()
+        ),
+        '<span class="edit-link">',
+        '</span>'
+      );
+    ?>
+  </footer>
+</article>
